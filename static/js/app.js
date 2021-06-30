2fc99e80eb5ebf883bd2f09fa1af28a2b33c7817
andrew bly <andrewbly36@gmail.com>
2021-06-30 12:29:14 -0400

    added city filter

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,12 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 tableData.forEach((ufoReport) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoReport).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
+  var row = tbody.append("tr");
+  Object.entries(ufoReport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
   });
+});
 
 
 var button = d3.select(".btn-default");
@@ -19,30 +19,51 @@ var form = d3.select(".form-control");
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
+//mindate function
+// function minDate() {
+//   let datetimeData = tableData.forEach(ufoReport => Date.parse(ufoReport.datetime))
+//   return Math.min(datetimeData)
+// }
+
+//maxdate function
+// function maxDate() {
+//   let datetimeData = tableData.forEach(ufoReport => Date.parse(ufoReport.datetime))
+//   return Math.max(datetimeData)
+// }
 
 function runEnter() {
 
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select(".form-control");
+  // Select the input element and get the raw HTML node
+  var inputDate = d3.select("#datetime");
+  var inputCity = d3.select('#city');
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+  // Get the value property of the input element
+  var inputDateValue = inputDate.property("value");
+  var inputCityValue = inputCity.property("value").toLowerCase();
 
-    console.log(inputValue);
+  // d3.selectAll('p').text('');
 
-    var filteredData = tableData.filter(event => event.datetime === inputValue);
-    
-    d3.select("tbody").html("");
+  var filteredData = tableData
+  if (inputDateValue) {
+    filteredData = filteredData.filter(event => event.datetime === inputDateValue);
+  }
+  else if (inputCityValue) {
+    filteredData = filteredData.filter(event => event.city === inputCityValue)
+  }
 
-    filteredData.forEach((ufoReport) => {
-        var row = tbody.append("tr");
-        Object.entries(ufoReport).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-        });
+  d3.select("tbody").html("");
+
+  filteredData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
 };
-  
+
+
+
