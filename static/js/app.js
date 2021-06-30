e37605b5e70c571ea903de5a34c4871e785f5b43
Andrew Bly <Andrewbly36@gmail.com>
2021-06-30 09:28:31 -0400

    initialized

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+
+
+tableData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+
